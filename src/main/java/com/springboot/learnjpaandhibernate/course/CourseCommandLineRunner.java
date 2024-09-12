72b432d14ce194075b3ed4b50521cd73305ef5d2
Ankita Saklani <saklani.ankita27@gmail.com>
2024-09-12 15:05:00 -0500

    Added more methods in CourseSpringDataJpaRepository.java class

--- src/main/java/com/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -16,27 +16,27 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Autowired
-    private CourseSpringJdbcRepository repository;
+//    @Autowired
+//    private CourseSpringJdbcRepository repository;
 
 //    @Autowired
 //    private CourseJpaRepository repository;
 
-//    @Autowired
-//    private CourseSpringDataJpaRepository repository;
+    @Autowired
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn Spring now!", "ankita"));
-        repository.insert(new Course(2, "Learn Spring Boot now!", "saklani"));
-        repository.insert(new Course(3, "Learn Java now!", "ankita"));
-        repository.insert(new Course(4, "Learn Python now!", "saklani"));
-//        repository.save(new Course(1, "Learn Java now!", "ankita"));
-//        repository.save(new Course(2, "Learn Python now!", "saklani"));
-//        repository.save(new Course(3, "Learn Spring now!", "ankita"));
-//        repository.save(new Course(4, "Learn Spring Boot now!", "saklani"));
-//        repository.save(new Course(5, "Learn Docker now!", "ankita"));
-//        repository.save(new Course(6, "Learn Junit now!", "saklani"));
+//        repository.insert(new Course(1, "Learn Spring now!", "ankita"));
+//        repository.insert(new Course(2, "Learn Spring Boot now!", "saklani"));
+//        repository.insert(new Course(3, "Learn Java now!", "ankita"));
+//        repository.insert(new Course(4, "Learn Python now!", "saklani"));
+        repository.save(new Course(1, "Learn Java now!", "ankita"));
+        repository.save(new Course(2, "Learn Python now!", "saklani"));
+        repository.save(new Course(3, "Spring now!", "ankita"));
+        repository.save(new Course(4, "Learn Spring Boot now!", "saklani"));
+        repository.save(new Course(5, "Learn Docker now!", "ankita"));
+        repository.save(new Course(6, "Junit now!", "saklani"));
 
 
         logger.info(repository.findById(1L).toString());
@@ -44,6 +44,12 @@ public class CourseCommandLineRunner implements CommandLineRunner {
         repository.deleteById(2L);
 
         logger.info(repository.findByAuthor("ankita").toString());
-        logger.info(repository.findByName("Learn Spring now!").toString());
+        logger.info(repository.findByName("Spring now!").toString());
+
+        logger.info(repository.findByNameAndAuthor("Learn Docker now!","ankita").toString());
+        logger.info(repository.findByNameOrAuthor("Learn Python now!","ankita").toString());
+
+        logger.info(repository.fetchCoursesByName("Junit now!").toString());
+        logger.info(repository.fetchCoursesBySuffixName("now!").toString());
     }
 }
--- src/main/java/com/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ src/main/java/com/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -2,6 +2,8 @@ package com.springboot.learnjpaandhibernate.course.springdatajpa;
 
 import com.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,13 @@ public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Lon
 
     List<Course> findByAuthor(String author);
     List<Course> findByName(String name);
+    List<Course> findByNameAndAuthor(String name, String author);
+    List<Course> findByNameOrAuthor(String name, String author);
+
+    @Query("Select c from Course c where c.name = ?1")
+    List<Course> fetchCoursesByName(String name);
+
+    @Query("Select c from Course c where c.name like %:suffix%")
+    List<Course> fetchCoursesBySuffixName(@Param("suffix") String name);
 
 }
